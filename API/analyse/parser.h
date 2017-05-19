e6a0381cdd66119a7887d3a908127761915f3c40
ELKAISSI SOUHAIL <elkaissimath@gmail.com>
2017-05-20 00:00:08 +0100

    buildding Parser

--- API/analyse/parser.h
+++ API/analyse/parser.h
@@ -1,4 +1,23 @@
 
 
+enum tree {DECLVAR , DECFUNCT , AFFICHAGE , AFFECTATION , CONDITION,BOUCLE,USEFUNC};
+
+typedef struct Tree{
+    enum tree type;
+    char *name;
+    
+}Tree;
 
 
+
+void Parser(Tokens *toks)
+{
+    Tokens *tmp = toks;
+    Token *tok = NULL;// ou on va stocker notre element
+    while(tmp)
+    {
+        tok = tmp->this;//l element dans le noeud acctuel
+        tmp = tmp->svt;//passer à l element suivant
+    }  //fin while
+
+}//fin fonction
--- sks.c
+++ sks.c
@@ -5,7 +5,8 @@ int main(int argc,char **args)
 {
     char *chaine = readFromFile("TEST/test.sks",chaine);//lecture du fichier
     char m[300];
-                   Tokens *t = Lexer(chaine);     
+                   Tokens *toks = Lexer(chaine);     
+                   Parser(toks);
     copyToFile("TEST/out",calculerExpressionNv1(chaine,m));//copier le resultat dans le fichier
     return 0;
 }
